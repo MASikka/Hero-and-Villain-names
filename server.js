14b6d5afc150125df3da311879bbc8b4ac25813f
Marek <mareksikka@gmail.com>
2021-09-02 17:23:41 +0300

    Hero and Villain names

---
+++ server.js
@@ -0,0 +1,8 @@
+var superheroes = require('superheroes');
+const supervillains = require('supervillains');
+
+var mySuperHeroName = superheroes.random();
+console.log(mySuperHeroName);
+
+console.log(supervillains.random());
+console.log(supervillains.all);
